2befcae5d3d4637605d8f4df3598ad4c170507e4
Laurie Kirkcaldy - Shadow <lauriekirkcaldy@gmail.com>
2017-08-12 13:28:59 +0100

    Working find and resize

--- WindowOrganiser/Form1.cs
+++ WindowOrganiser/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,20 @@ namespace WindowOrganiser
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			foreach(KeyValuePair<IntPtr, string> window in WindowFinder.GetOpenWindows()) {
+				IntPtr handle = window.Key;
+				string title = window.Value;
+
+				if (title.Contains("Firefox") && title.Contains("Messenger")) {
+					Console.WriteLine("{0}: {1}", handle, title);
+
+					WindowMover.MoveWindow(handle , 80 , 80 , 400 , 400 , true);
+				}
+			}
         }
     }
 }
---
+++ WindowOrganiser/WindowMover.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowOrganiser {
+	class WindowMover {
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern bool MoveWindow(IntPtr hwnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+		// bRepaint specifies whether to tell the window that it has been moved/resized so it can re-distribute it's stuff on screen (most things catch the move anyway)
+
+
+		[DllImport("user32.dll")]
+		public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+		// nCmdShow see https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
+	}
+}
